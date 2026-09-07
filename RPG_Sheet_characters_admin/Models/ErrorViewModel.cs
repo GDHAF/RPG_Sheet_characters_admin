5124f13793d1aca383d1363dad7be72fcaba88bf
Henrique AF <henriqueaf22@gmail.com>
2026-09-07 19:13:01 -0300

    Adicionar arquivos de projeto.

---
+++ RPG_Sheet_characters_admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPG_Sheet_characters_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
